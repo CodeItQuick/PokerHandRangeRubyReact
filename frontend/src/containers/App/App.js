d984db0a48da13bdd54d4ed8b8e175ba3f57c4d9
CodeItQuick <evanontario@hotmail.com>
2020-02-09 18:06:38 -0400

    first commit on creating user interface

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -3,12 +3,14 @@ import logo from '../../logo.svg';
 import './App.css';
 import Board from '../../components/board/board';
 import MainPage from '../MainPage/index';
+import UserLogin from '../UserLogin/UserLogin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <MainPage></MainPage>
+        <UserLogin></UserLogin>
       </header>
     </div>
   );
