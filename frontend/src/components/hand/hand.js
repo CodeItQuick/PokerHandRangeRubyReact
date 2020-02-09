9695b9b55fe6fd0838c0cf40d69eaa724925c595
Dylan Ellison <dylancellison@gmail.com>
2020-02-09 16:20:25 -0600

    Add destructuring to props in Hand component

--- frontend/src/components/hand/hand.js
+++ frontend/src/components/hand/hand.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { Grid, Button } from 'semantic-ui-react';
-import styled from 'styled-components';
-
+import React from "react";
+import { Grid, Button } from "semantic-ui-react";
+import styled from "styled-components";
 
 function Hand(props) {
+  const { handOnClick, cardOne, cardTwo, suit, classColor } = props;
 
-    return (
-        <Grid.Column><Button onClick={props.handOnClick} name={props.cardOne + props.cardTwo + props.suit} className={props.classColor(props.cardOne, props.cardTwo, props.suit)}>{props.cardOne}{props.cardTwo}{props.suit}</Button></Grid.Column>
-    );
+  return (
+    <Grid.Column>
+      <Button
+        onClick={handOnClick}
+        name={cardOne + cardTwo + suit}
+        className={classColor(cardOne, cardTwo, suit)}
+      >
+        {cardOne}
+        {cardTwo}
+        {suit}
+      </Button>
+    </Grid.Column>
+  );
 }
 
 export default Hand;
