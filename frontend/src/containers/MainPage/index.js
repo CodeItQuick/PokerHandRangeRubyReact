220b8af5cfa60064d2ffe9b488a2e0c65fcf3ecb
CodeItQuick <evanontario@hotmail.com>
2020-04-14 00:27:24 -0300

    refactoring

--- frontend/src/containers/MainPage/index.js
+++ frontend/src/containers/MainPage/index.js
@@ -20,10 +20,11 @@ import {
 import { getHandRange, setHandRangeSelect, setHandRange } from "./actions.js";
 import reducer from "./reducer.js";
 import { useInjectReducer } from "../../HOC/useInjectReducer.js";
-import { initialState } from "./reducer.js";
+
 import styled from "styled-components";
 import StreetSelector from "./StreetSelector";
-import BoardCards from "./BoardCards";
+import BoardCards from "./InputForm/BoardCards";
+import { InputForm } from "./InputForm";
 
 const key = "global";
 
@@ -32,7 +33,7 @@ const MainPage = ({ wholeRange, ranges, mode, rangeColors }) => {
   const dispatch = useDispatch();
   const [mouseDownState, updateMouseDownState] = useState(false);
 
-  const handleStreet = (e, data) => {
+  const onHandleStreetHandler = (e, data) => {
     dispatch(setHandRangeSelect({ name: data.name, value: data.value }));
   };
 
@@ -41,59 +42,24 @@ const MainPage = ({ wholeRange, ranges, mode, rangeColors }) => {
     return data.cards;
   };
 
+  const onChangeStreetHandler = e => updateDeadCards(e.target.value);
+
   const [deadCards, updateDeadCards] = useState();
 
   return (
     <Container stackable={true}>
-      <Row>
-        <Col>
-          <Form>
-            <Col
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between"
-              }}
-            >
-              <Form.Input
-                label="Dead Cards"
-                placeholder="Enter dead cards, for example Ah, As, 2c"
-                style={{ width: 300 }}
-                name="deadcards"
-                onChange={e => updateDeadCards(e.target.value)}
-              ></Form.Input>
-              <Form.Button type="submit">Submit</Form.Button>
-            </Col>
-            <Col>
-              <BoardCards cardsFlipped={deadCards}></BoardCards>
-            </Col>
-            <Col>
-              <StreetSelector
-                handleStreet={handleStreet}
-                street={mode.street}
-              ></StreetSelector>
-            </Col>
-            <Col>
-              <Button label="assign"></Button>
-              <Button label="Ranges"></Button>
-            </Col>
-            <Col>
-              <Button label="Clear Selection"></Button>
-              <Button label="Clear Suits"></Button>
-              <Button label="Split Suits"></Button>
-            </Col>
-          </Form>
-        </Col>
-        <Col>
-          <Board
-            onMouseOverHandler={onMouseOverHandler}
-            rangeColors={rangeColors}
-          ></Board>
-          <Col>
-            <BoardLegend wholeRange={wholeRange} mode={mode}></BoardLegend>
-          </Col>
-        </Col>
-      </Row>
+      <InputForm
+        onHandleStreetHandler={onHandleStreetHandler}
+        onChangeStreetHandler={onChangeStreetHandler}
+        deadCards={deadCards}
+        mode={mode}
+        StreetSelector={StreetSelector}
+      />
+      <Board
+        onMouseOverHandler={onMouseOverHandler}
+        rangeColors={rangeColors}
+      ></Board>
+      <BoardLegend wholeRange={wholeRange} mode={mode}></BoardLegend>
     </Container>
   );
 };
