86e6b5532ed1e6e56f1de7ff03413151d8db4ea7
CodeItQuick <evanontario@hotmail.com>
2020-06-26 18:19:58 -0300

    beef up deploy

--- deployment_scripts/compile-assets.sh
+++ deployment_scripts/compile-assets.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
 # Go to the deployment directory
-cd /var/www/ec2-user/deployment
+sudo cd /var/www/ec2-user/deployment
 
 # Load environment variables
 source /etc/profile
 
 # Run asset precompilation
 # - No need to run assets:clobber because this is a new folder. There should be none. We compile assets from the beginning in all deployments.
-RAILS_ENV=production bundle exec rake assets:precompile
+sudo RAILS_ENV=production bundle exec rake assets:precompile
