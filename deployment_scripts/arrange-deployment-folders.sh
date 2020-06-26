ac97887c482368f1fa5fb500b8875d858ded3714
CodeItQuick <evanontario@hotmail.com>
2020-06-26 19:41:40 -0300

    more working now

--- deployment_scripts/arrange-deployment-folders.sh
+++ deployment_scripts/arrange-deployment-folders.sh
@@ -9,4 +9,4 @@ sudo mv /var/www/ec2-user/deployment /var/www/ec2-user/prev-deployment
 # Create new deployment folder and make nginx owner
 sudo mkdir /var/www/ec2-user/deployment
 
-sudo chown ec2-user:ec2-user /var/www/pisirpaylas/deployment
+sudo chown ec2-user:ec2-user /var/www/ec2-user/deployment
