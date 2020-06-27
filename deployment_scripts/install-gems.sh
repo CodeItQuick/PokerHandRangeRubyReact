668232970cf2e53b4070ea157f2dee0c75a58e78
CodeItQuick <evanontario@hotmail.com>
2020-06-27 14:29:59 -0300

    switch to rvm

--- deployment_scripts/install-gems.sh
+++ deployment_scripts/install-gems.sh
@@ -4,7 +4,7 @@
 cd /var/www/ec2-user/deployment/backend
 
 # Get temporaray credentials for AWS CodeCommit
-# - Remember that the instance must have access rights to the CodeCommit repositories in Gemfile.
+# - Remember that the ec2 instance must have access rights to the CodeCommit repositories in Gemfile.
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 
@@ -14,5 +14,6 @@ git config --global credential.UseHttpPath true
 # - Without development and test gems
 # - Using gemfile in current directory
 # - Also quietly. No need to generate all logs.
+rvm use 2.7.1
 gem install bundler
 RAILS_ENV=production bundle install --binstubs /var/www/ec2-user/shared/bin --gemfile ./Gemfile --path /var/www/ec2-user/shared/bundle --without development test --deployment --quiet
