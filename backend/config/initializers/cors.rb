4001e2adc7af2648b0ed576d542f9be57f058faa
CodeItQuick <evanontario@hotmail.com>
2021-02-21 16:40:29 -0400

    minor revisions

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -26,9 +26,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
     allow do
-      origins 'https://www.poker-range-appalyzer.com'
+      origins 'www.poker-range-appalyzer.com'
       resource '*',
         headers: :any,
         methods: %i(get post put patch delete options head)
+        credentials: true
     end
   end
